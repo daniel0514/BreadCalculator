cb04803a848710f061bb4bcfd5ba85b4cb4245b6
daniel0514 <daniel0514@gmail.com>
2015-05-14 17:23:01 -0700

    use static to store HashMap instead of files for greater speed

--- app/src/main/java/cal/bread/dan/breadcalculator2/BreadList.java
+++ app/src/main/java/cal/bread/dan/breadcalculator2/BreadList.java
@@ -1,6 +1,7 @@
 package cal.bread.dan.breadcalculator2;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,6 +18,9 @@ import java.util.List;
  */
 public class BreadList {
     LinkedList<Bread> breadList;
+    static LinkedHashMap<String, Integer> trainHM;
+    static LinkedHashMap<String, Integer> percentHM;
+    static LinkedHashMap<String, Integer> starHM;
     int star;
     int size = 0;
     int cost = 0;
@@ -26,11 +30,13 @@ public class BreadList {
 
     //Constructor
     public BreadList(int star){
+        setUpHashMaps();
         this.star = star;
         breadList = new LinkedList<Bread>();
     }
     //Constructor
     public BreadList(int star, Bread bread){
+        setUpHashMaps();
         this.star = star;
         breadList = new LinkedList<>();
         breadList.add(bread);
@@ -41,6 +47,7 @@ public class BreadList {
     }
     //Constructor
     public BreadList(int star, String bread){
+        setUpHashMaps();
         this.star = star;
         Bread b = new Bread(bread);
         breadList = new LinkedList<>();
@@ -52,6 +59,7 @@ public class BreadList {
     }
     //Constructor
     public BreadList(BreadList bList){
+        setUpHashMaps();
         this.star = bList.getStar();
         this.breadList = new LinkedList<>();
         for(Bread b: bList.getBreads()){
@@ -116,6 +124,106 @@ public class BreadList {
 
     public int getSize(){
         return breadList.size();
+    }
+
+    /**
+     * Set Up HashMaps for Bread Information
+     */
+    private void setUpHashMaps(){
+        //Set Up HashMap for bread star level if HM is not set up already
+        if(starHM == null){
+            setUpStarHM();
+        }
+        //Set Up HashMap for bread train amount if HM is not set up already
+        if(trainHM == null){
+            setUpTrainHM();
+        }
+        //Set Up HashMap for bread percentage if HM is not set up already
+        if(percentHM == null){
+            setUpPercentHM();
+        }
+    }
+
+    //Setting up the HashMap for Information of Bread Level (Stars)
+    private void setUpStarHM(){
+        starHM = new LinkedHashMap<>(23);
+        starHM.put("Macaroon", 6);
+        starHM.put("Hamburger", 6);
+        starHM.put("Special Donut", 6);
+        starHM.put("Strawberry Pie", 5);
+        starHM.put("Chocolate", 4);
+        starHM.put("Pizza", 5);
+        starHM.put("Choco Cup Cake", 4);
+        starHM.put("Shamrock Cup Cake", 4);
+        starHM.put("Strawberry Donut", 5);
+        starHM.put("Cream Bread", 4);
+        starHM.put("Christmas Cake", 4);
+        starHM.put("Sandwich", 4);
+        starHM.put("Big Choco Cake", 4);
+        starHM.put("Rice Donut", 4);
+        starHM.put("Croissant", 3);
+        starHM.put("Snake Wrap", 3);
+        starHM.put("Jelly Roll", 3);
+        starHM.put("Bread", 2);
+        starHM.put("Hot Dog", 2);
+        starHM.put("Morning Bread", 1);
+        starHM.put("Choco Donut", 2);
+        starHM.put("Sausage Bread", 1);
+        starHM.put("Donut", 1);
+    }
+    //Setting up the HashMap for Information of Bread Training amount
+    private void setUpTrainHM(){
+        trainHM = new LinkedHashMap<>(23);
+        trainHM.put("Macaroon", 600);
+        trainHM.put("Hamburger", 480);
+        trainHM.put("Special Donut", 360);
+        trainHM.put("Strawberry Pie", 330);
+        trainHM.put("Chocolate", 280);
+        trainHM.put("Pizza", 264);
+        trainHM.put("Choco Cup Cake", 240);
+        trainHM.put("Shamrock Cup Cake", 200);
+        trainHM.put("Strawberry Donut", 198);
+        trainHM.put("Cream Bread", 180);
+        trainHM.put("Christmas Cake", 150);
+        trainHM.put("Sandwich", 144);
+        trainHM.put("Big Choco Cake", 140);
+        trainHM.put("Rice Donut", 108);
+        trainHM.put("Croissant", 100);
+        trainHM.put("Snake Wrap", 80);
+        trainHM.put("Jelly Roll", 60);
+        trainHM.put("Bread", 50);
+        trainHM.put("Hot Dog", 40);
+        trainHM.put("Morning Bread", 30);
+        trainHM.put("Choco Donut", 30);
+        trainHM.put("Sausage Bread", 24);
+        trainHM.put("Donut", 18);
+    }
+    //Setting up the HashMap for Information of Bread Percentage
+    private void setUpPercentHM(){
+        percentHM = new LinkedHashMap<>(23);
+        percentHM.put("Macaroon", 0);
+        percentHM.put("Hamburger", 20);
+        percentHM.put("Special Donut", 40);
+        percentHM.put("Strawberry Pie", 0);
+        percentHM.put("Chocolate", 0);
+        percentHM.put("Pizza", 20);
+        percentHM.put("Choco Cup Cake", 15);
+        percentHM.put("Shamrock Cup Cake", 30);
+        percentHM.put("Strawberry Donut", 40);
+        percentHM.put("Cream Bread", 0);
+        percentHM.put("Christmas Cake", 25);
+        percentHM.put("Sandwich", 20);
+        percentHM.put("Big Choco Cake", 50);
+        percentHM.put("Rice Donut", 40);
+        percentHM.put("Croissant", 0);
+        percentHM.put("Snake Wrap", 20);
+        percentHM.put("Jelly Roll", 40);
+        percentHM.put("Bread", 0);
+        percentHM.put("Hot Dog", 20);
+        percentHM.put("Morning Bread", 0);
+        percentHM.put("Choco Donut", 40);
+        percentHM.put("Sausage Bread", 20);
+        percentHM.put("Donut", 40);
     }
 
 }
